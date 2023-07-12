35a7d89675bb95c82fbf66e67f08e3fef505824c
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-07-12 21:35:27 +0100

    Adding app service

--- terraform/app-service.tf
+++ terraform/app-service.tf
@@ -10,11 +10,38 @@ resource "azurerm_resource_group" "app" {
 resource "azurerm_service_plan" "app" {
   for_each = toset(var.locations)
 
-  name = format("app-%s-%s-%s", random_id.environment_id.hex, var.environment, each.value)
+  name = format("sp-%s-%s-%s", random_id.environment_id.hex, var.environment, each.value)
 
   resource_group_name = azurerm_resource_group.app[each.value].name
   location            = azurerm_resource_group.app[each.value].location
 
   os_type  = "Linux"
   sku_name = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  for_each = toset(var.locations)
+
+  name = format("app-%s-%s-%s", random_id.environment_id.hex, var.environment, each.value)
+
+  resource_group_name = azurerm_resource_group.app[each.value].name
+  location            = azurerm_resource_group.app[each.value].location
+
+  service_plan_id = azurerm_service_plan.app[each.value].id
+
+  https_only = true
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"             = azurerm_application_insights.ai[each.value].instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"      = azurerm_application_insights.ai[each.value].connection_string
+    "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
+  }
+
+  site_config {
+    ftps_state = "Disabled"
+
+    application_stack {
+      dotnet_version = "7.0"
+    }
+  }
 }
